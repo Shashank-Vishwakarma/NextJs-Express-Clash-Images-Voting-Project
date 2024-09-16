9ca44153e15caec0c6ec9b585ac3669fb1990f46
Shashank <shashank78680@gmail.com>
2024-09-16 18:49:08 +0530

    added not found svg

--- nextjs-clash-images-voting/src/app/not-found.tsx
+++ nextjs-clash-images-voting/src/app/not-found.tsx
@@ -1,14 +1,19 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function NotFound() {
     return (
-        <div className="w-full h-screen justify-center items-center flex flex-col">
-            <h2 className="text-5xl font-bold m-2">Not Found</h2>
-            <p className="text-3xl m-2">Could not find requested resource</p>
-            <div className="m-2">
+        <div className="w-full h-screen items-center flex flex-col">
+            <Image
+                src={"/not-found.svg"}
+                width={500}
+                height={500}
+                alt="Not Found"
+            />
+            <div>
                 <Link href={"/"}>
-                    <Button>Return to Home</Button>
+                    <Button className="w-48 h-12">Return to Home</Button>
                 </Link>
             </div>
         </div>
