b4af78db197d45278a7d543d43336e0bbfafc4d7
Shashank <shashank78680@gmail.com>
2024-09-17 16:23:32 +0530

    signup email template setup with ejs

--- express-clash-images-voting/src/services/email.ts
+++ express-clash-images-voting/src/services/email.ts
@@ -1,13 +1,22 @@
 import { transporter } from "../config/nodemailer.config.js";
 import { ENV_VARS } from "../utils/envVariables.js";
+import ejs from "ejs";
+import path from "path";
+import { fileURLToPath } from "url";
 
-export async function sendEmail(to: string, subject: string, body: string) {
+export async function sendEmail(to: string, subject: string) {
     try {
+        const __dirname = path.dirname(fileURLToPath(import.meta.url));
+        const htmlBody = await ejs.renderFile(
+            path.resolve(__dirname, "../views/emails/welcomeEmail.ejs"),
+            { name: to }
+        );
+
         const info = await transporter.sendMail({
             from: ENV_VARS.FROM_EMAIL,
             to,
             subject,
-            html: body,
+            html: htmlBody,
         });
 
         return { messageId: info.messageId };
